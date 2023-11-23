a2b7934476a6d50fb573feb9b3d8c06a6b13daf8
Rafaeltheraven <rafael@dulfer.be>
2023-11-23 14:57:31 +0100

    fix the preview script for the new setup

--- examples/preview.rs
+++ examples/preview.rs
@@ -1,37 +1,36 @@
-extern crate piston_window;
 
 use piston_window::*;
-use colourado::*;
+use colourado_iter::{PaletteType, ColorPalette};
 use std::env;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
 
 	if args.len() != 4 {
-		panic!("You must supply exactly 2 parameters. The palette type and the number of colors to generate!");
+		panic!("You must supply exactly 3 parameters. The palette type, the type of spread and the number of colors to generate!");
 	}
 
-	let num_colors = args[2].parse().unwrap_or(4);
 	let palette_type = match args[1].to_lowercase().as_ref() {
 		"pastel" => PaletteType::Pastel,
 		"dark" => PaletteType::Dark,
 		_ => PaletteType::Random
 	};
-	let adjacent = match args[3].to_lowercase().as_ref() {
-		"adjacent" => true,
-		"spread" => false,
-		_ => false
-	};
 
-	let palette = ColorPalette::new(num_colors, palette_type, adjacent);
+	let num_colors = args[2].parse().unwrap_or(4);
+	
+	let adjacent = matches!(args[3].to_lowercase().as_ref(), "adjacent");
+
+	let palette = ColorPalette::new(palette_type, adjacent, rand::thread_rng());
+
+	let colors: Vec<_> = palette.take(num_colors).collect();
 
     let mut window: PistonWindow = WindowSettings::new("Color palette preview", [1280, 720]).exit_on_esc(true).build().unwrap();
     while let Some(event) = window.next() {
-		window.draw_2d(&event, |context, graphics| {
+		window.draw_2d(&event, |context, graphics, _| {
 			clear([1.0; 4], graphics);
 
-			for (i, color) in (&palette.colors).iter().enumerate() {
-				rectangle([color.red, color.green, color.blue, 1.0], 
+			for (i, color) in colors.iter().enumerate() {
+				rectangle(color.to_rgba_array(), 
 					  [(120.0 * i as f64) % 1200.0, ((120.0 * i as f64) / 1200.0).floor() * 120.0, 100.0, 100.0],
                       context.transform,
                       graphics);
